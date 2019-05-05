8338c6b61c07a3f575cfc7e4c522203000fcd489
yayin <yayin.chen314@gmail.com>
2019-05-05 13:12:49 +0800

    week3

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,10 @@
 function isPalindromes(str) {
-  console.log(str);
+  for (let i = 1; i <= (str.length) / 2; i += 1) {
+    if (str[i - 1] !== str[str.length - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isPalindromes;
